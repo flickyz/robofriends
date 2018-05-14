1f5d697a109c356e37e89bc6f106336d99293466
flicky <flicky@flicky-MacBook-Pro.local>
2018-05-14 12:29:20 +1200

    adding title and a search bar

---
+++ src/CardList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Card from './Card';
+
+
+const CardList = ({ robots }) => {
+	return (
+		<div className = 'tc'>
+			{
+				robots.map ((user, i) => {
+					return (
+						<Card
+						key = {i} 
+						name = {user.name} 
+						email = {user.email} 
+						id = {user.id} 
+						/>
+					);	
+				})
+			}
+		</div>
+	);
+}
+
+export default CardList;
---
+++ src/SearchBox.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const SearchBox = () => {
+
+	return (
+
+		<div className = 'pa2'>
+			<input 
+			className = 'pa3 ba b--green bg-light-green' 
+			type = 'search' 
+			placeholder = 'search your robofriends' 
+			/>
+			</div>
+		);
+}
+
+export default SearchBox;
